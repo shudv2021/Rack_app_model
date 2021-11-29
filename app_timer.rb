13d94ccc427b86d90037949ac489274d4ae3fb63
shudv2021 <shudv@yandex.ru>
2021-11-29 20:25:24 +0300

    reduce method

--- app_timer.rb
+++ app_timer.rb
@@ -1,5 +1,4 @@
 require_relative 'time_convertor'
-
 class AppTimer
 
   def call(env)
@@ -9,23 +8,15 @@ class AppTimer
   private
 
   def request_handler(request)
-
-    if request.path_info != '/time'
-      response(404, 'Check path which you are input')
-    else
-      converted_time = TimeConvertor.new(request.params)
-      converted_time.call
-      if converted_time.success?
-        response(200, converted_time.time_by_pattern )
-      else
-        response(400, "Unknown format #{ converted_time.format_error}")
-      end
-    end
-
+    return response(404, 'Check path you input') unless request.path_info == '/time'
+    converted_time = TimeConvertor.new(request.params)
+    converted_time.call
+    response(400,"Unknown format #{ converted_time.format_error}") unless converted_time.success?
+    response(200, "#{converted_time.time_by_pattern}" ) if converted_time.success?
   end
 
   def response(status, body)
-   Rack::Response.new([body], status, { 'Content-Type' => 'text/plain' }).finish
+    Rack::Response.new([body], status, { 'Content-Type' => 'text/plain' }).finish
   end
 
 end
